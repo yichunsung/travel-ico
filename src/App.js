a2ea8846c47b0f6660b381d1ad4303f299aaf011
Yichun Sung <ycsung.r@gmail.com>
2019-12-21 02:50:49 +0800

    init :tada::tada::tada::tada::tada:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
+import Home from './home'
+
+const { useState } = React;
+
+const open = {
+  width: '250px'
+}
+const close = {
+  width: '0px'
+}
+
 function App() {
+  const [sideBarShow, setSideBarShow] = useState(false);
+
+  /*
+  let haa = ()=> {
+    if (data === 'Yoooo') {
+      setData('Hello')
+    } else {
+      setData('Yoooo')
+    }
+  }
+  */
+
+  let closeNav = ()=> {
+    setSideBarShow(false)
+  }
+
+  let openOrCloseNav = ()=> {
+    if (sideBarShow) {
+      setSideBarShow(false)
+    } else {
+      setSideBarShow(true)
+    }
+  } 
+  
+  let d = [];
+  /*
+  for (let i = 0; i < arrayData.length; i++) {
+    d.push(<div>id = { arrayData[i]} </div>)
+  }*/
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <div className="App">
+        <div
+          className="sidebar" 
+          style={{
+            width: sideBarShow && '250px'
+          }}>
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Clients</a>
+          <a href="#">Contact</a>
+        </div>
+        <div className="openbtn" onClick={ ()=>{ openOrCloseNav() } }>☰</div>  
+        <div 
+          className="main"
+          style={{
+            marginLeft: sideBarShow && '250px'
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Route path="/" component={Home} />
+        </div>
+      </div>
+     </Router>
   );
 }
 
